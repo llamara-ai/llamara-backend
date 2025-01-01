1ed39f595cb6713c90f651d06a46686233d95db8
Florian Hotze <dev@florianhotze.com>
2025-01-01 15:58:41 +0100

    Fix test build
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- src/test/java/com/github/llamara/ai/internal/internal/security/user/TestUserRepository.java
+++ src/test/java/com/github/llamara/ai/internal/internal/security/user/TestUserRepository.java
@@ -1,13 +1,16 @@
 package com.github.llamara.ai.internal.internal.security.user;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Typed;
 
 /**
  * Extends {@link UserRepository} for modifying visibility of constructor and methods to access them
  * in tests.
  */
+@Typed(TestUserRepository.class)
 @ApplicationScoped
 public class TestUserRepository extends UserRepository {
+    @Override
     public void init() {
         super.init();
     }
