llamara-ai/llamara-backend

commit c8c64e14b1692e00e93faa57c6692175cc187093

Adjust bean initialization methods to follow CDI best practices (#83)
Signed-off-by: Florian Hotze <dev@florianhotze.com>

src/main/java/com/github/llamara/ai/internal/security/user/UserRepository.java
@@ -21,11 +21,12 @@ package com.github.llamara.ai.internal.security.user;
 
 import com.github.llamara.ai.internal.security.Users;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import io.quarkus.runtime.Startup;
+import io.quarkus.logging.Log;
 
 /**
  * Hibernate ORM {@link PanacheRepository} for {@link User}.
@@ -34,10 +35,11 @@ import io.quarkus.runtime.Startup;
  */
 @ApplicationScoped
 public class UserRepository implements PanacheRepository<User> {
-    @Startup
+    @PostConstruct
     @Transactional
     void init() {
         if (findByUsername(Users.ANY_USERNAME) == null) {
+            Log.info("Initially persisting user ANY to database ...");
             persist(Users.ANY);
         }
     }

src/main/java/com/github/llamara/ai/internal/knowledge/KnowledgeManagerImpl.java
@@ -49,6 +49,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -104,8 +105,8 @@ class KnowledgeManagerImpl implements KnowledgeManager {
         this.embeddingStorePermissionMetadataManager = embeddingStorePermissionMetadataManager;
     }
 
-    @Startup
-    void init() {
+    @PostConstruct
+    private void init() {
         try {
             if (Utils.isOsUnix()) {
                 FileAttribute<Set<PosixFilePermission>> attr =

commit 19149422f72d052b6a7900e21a4884d0b15a4c05

Fix ANY user not initialized at Quarkus startup (#93)
Regression from #83.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

src/main/java/com/github/llamara/ai/internal/security/user/UserRepository.java
@@ -21,12 +21,12 @@ package com.github.llamara.ai.internal.security.user;
 
 import com.github.llamara.ai.internal.security.Users;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.logging.Log;
+import io.quarkus.runtime.Startup;
 
 /**
  * Hibernate ORM {@link PanacheRepository} for {@link User}.
@@ -35,7 +35,7 @@ import io.quarkus.logging.Log;
  */
 @ApplicationScoped
 public class UserRepository implements PanacheRepository<User> {
-    @PostConstruct
+    @Startup // initialize user ANY at startup
     @Transactional
     void init() {
         if (findByUsername(Users.ANY_USERNAME) == null) {